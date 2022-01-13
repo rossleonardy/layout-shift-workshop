68b94aa8b348d68c70c98fd0aab473c4ef94f1a2
Ana Maršić <ana.marsicv@gmail.com>
2022-01-13 18:09:51 +0100

    Offer card list skeleton

--- src/OfferCardList/index.tsx
+++ src/OfferCardList/index.tsx
@@ -18,31 +18,36 @@ interface OfferCardProps {
 
 export const OfferCard = ({uuid}: OfferCardProps) => {
   const { loading, error, data } = useFetch<Offer>(`http://localhost:8787/offer/${uuid}`, {}, [])
+  const offerCardData: Partial<Offer> = loading ? {
+    amount: "",
+    callouts: [""],
+    flag: "",
+    monthly: "",
+    term: ""
+  } : data as Offer
 
-  if (loading) {
-    return <div>Loading</div>
-  } else if (!data || error) {
+  if (error) {
     return <div>Error</div>
   } else {
     return <div className="offerCard">
-      <div className="flag">{data.flag}</div>
+      <div className="flag">{offerCardData.flag}</div>
       <div className="offerBody">
         <img className="wordmark" src={`http://localhost:8787/wordmark/${uuid}`} />
           <div className="amount">
             <h4>Amount</h4>
-            <div>{data.amount}</div>
+            <div>{offerCardData.amount}</div>
           </div>
           <div className="term">
             <h4>Term</h4>
-            <div>{data.term}</div>
+            <div>{offerCardData.term}</div>
           </div>
           <div className="monthly">
             <h4>Monthly</h4>
-            <div>{data.monthly}</div>
+            <div>{offerCardData.monthly}</div>
           </div>
           <div className="continue">CONTINUE</div>
           <div className="callouts">
-            {data.callouts.map((callout, i) => <div key={i} className="calloutPill">{callout}</div>)}
+            {offerCardData?.callouts?.map((callout, i) => <div key={i} className="calloutPill">{callout}</div>)}
           </div>
       </div>
       <div></div>
@@ -54,7 +59,9 @@ export const OfferCardList = () => {
   const { loading, error, data } = useFetch<string[]>('http://localhost:8787/offerList', {}, [])
 
   if (loading) {
-    return <div>Loading</div>
+    return <div className="offerCardList">
+    {["uuid1", "uuid2", "uuid3"].map((uuid) => <OfferCard uuid={uuid} key={uuid} />)}
+  </div>
   } else if (!data || error) {
     return <div>Error</div>
   } else {
