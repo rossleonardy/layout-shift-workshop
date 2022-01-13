627a8ce756e9ea6976b7a1c1e4c8c66d0fdb4664
Michelle Berger <bergerm613@gmail.com>
2022-01-13 12:14:26 -0500

    layout updates

--- src/Headline/index.tsx
+++ src/Headline/index.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import useFetch from 'use-http'
-import './style.css'
+import React from "react";
+import useFetch from "use-http";
+import "./style.css";
 
-interface HeadlineContent {
-  headline: string
-  subhead: string
+export interface HeadlineContent {
+  headline: string;
+  subhead: string;
 }
 
-export const Headline = () => {
-  const { loading, error, data } = useFetch<HeadlineContent>(`http://localhost:8787/headlineContent`, {}, [])
-
-  if (loading) {
-    return <div>Loading</div>
-  } else if (!data || error) {
-    return <div>Error</div>
-  } else {
-    return <div className="headline">
-      <h1>{data.headline}</h1>
-      <h5>{data.subhead}</h5>
+export const Headline = ({
+  headlineData,
+}: {
+  headlineData: HeadlineContent;
+}) => {
+  return (
+    <div className="headline">
+      <h1>{headlineData.headline}</h1>
+      <h5>{headlineData.subhead}</h5>
     </div>
-  }
-}
+  );
+};
