460bc6e30e60aa1f6d4f4c2b827720968abdf4ca
Ana Maršić <ana.marsicv@gmail.com>
2022-01-13 17:54:46 +0100

    Headline min-width

--- src/Headline/index.tsx
+++ src/Headline/index.tsx
@@ -10,14 +10,20 @@ interface HeadlineContent {
 export const Headline = () => {
   const { loading, error, data } = useFetch<HeadlineContent>(`http://localhost:8787/headlineContent`, {}, [])
 
+
   if (loading) {
-    return <div>Loading</div>
+    return <div className="headline">
+      <div>Loading</div>
+    </div>
   } else if (!data || error) {
-    return <div>Error</div>
+    return <div className="headline">
+      <div>Error</div>
+    </div>
   } else {
     return <div className="headline">
       <h1>{data.headline}</h1>
       <h5>{data.subhead}</h5>
     </div>
   }
+
 }
