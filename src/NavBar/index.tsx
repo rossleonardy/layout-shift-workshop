d428ab3c518767e506603bd339824970791df3c5
Ana Maršić <ana.marsicv@gmail.com>
2022-01-13 17:41:20 +0100

    Img width and height

--- src/NavBar/index.tsx
+++ src/NavBar/index.tsx
@@ -3,8 +3,8 @@ import './style.css'
 
 export const NavBar = () => <div className="navbar">
   <div>
-    <img className="workshop" src="http://localhost:8787/assets/workshop.png" />
-    <img className="workshop small" src="http://localhost:8787/assets/workshop-small.png" />
+    <img className="workshop" src="http://localhost:8787/assets/workshop.png" width="128" height="128" />
+    <img className="workshop small" src="http://localhost:8787/assets/workshop-small.png" width="64" height="64" />
   </div>
   <ul>
     <li>Home</li>
